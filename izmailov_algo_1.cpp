598b9f161dca921603d31ea9ea37c111040a647f
MIZORIT-tango <mizmailovartem@gmail.com>
2025-09-12 14:26:20 +0300

    Добавил меню и получение выбранного действия

--- izmailov_algo_1.cpp
+++ izmailov_algo_1.cpp
@@ -5,6 +5,19 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to the application 'basic essences of pipeline transportation of gas or oil'\n";
+    while (true) {
+        std::cout << "Select an action:\n"
+            << "1. Add pipe\n"
+            << "2. Add CS\n"
+            << "3. Viewing all objects\n"
+            << "4. Edit pipe\n"
+            << "5. Edit CS\n"
+            << "6. Save\n"
+            << "7. Download\n"
+            << "0. Exit\n";
+        int menu_choose;
+        std::cin >> menu_choose;
+    }
 }
 
