5764c70659930cabe6f6a701f38326ba7e3c2ea0
MIZORIT-tango <mizmailovartem@gmail.com>
2025-09-13 14:42:36 +0300

    сделал создание трубы с проверкой ввода

--- izmailov_algo_1.cpp
+++ izmailov_algo_1.cpp
@@ -1,28 +1,29 @@
 ﻿#include <iostream>
 #include <string>
 #include <limits>
+#include <sstream>
 
 struct Pipe {
     std::string name= "0";
-    float lenth = 0.;
-    int diametr = 0;
+    float length = 0.;
+    int diameter = 0;
     bool status = false;
 
     bool switch_status(int o) {
         if (o == 1) {
             status = true;
-            std::cout << "Status: Under renovation\n";
+            std::cout << "Status: Under renovation\n\n";
             return status;
         }
         else {
             if (o == 2) {
                 status = false;
-                std::cout << "Status: normal\n";
+                std::cout << "Status: normal\n\n";
                 return status;
             }
             else {
                 std::cout << "Incorrect value! Try again\n";
-                std::cout << "Status is not changed\n";
+                std::cout << "Status is not changed\n\n";
                 return status;
             }
         }
@@ -66,8 +67,51 @@ void show_menu(Pipe& p, CompressStation& c) {
 
         switch (menu_choose) {
         case 1:
-            std::cout << "Enter name of pipe\n";
-            std::getline(std::cin, p.name);
+            {
+                bool type_error = false;  
+
+                std::string safe_name;
+                float safe_length = 0.0f;
+                int safe_diameter = 0;
+
+                std::cout << "Enter name of pipe:\n";
+                clearInputBuffer();
+                std::getline(std::cin, safe_name);
+
+                std::cout << "Enter length of pipe:\n";
+                std::cin >> safe_length;
+                if (std::cin.fail()) {
+                    std::cout << "Invalid input! Please enter a float.\n\n";
+                    clearInputBuffer();
+                    type_error = true;
+                }
+
+                if (!type_error) {
+                    std::cout << "Enter diameter of pipe:\n";
+
+                    clearInputBuffer();
+                    std::string input;
+                    std::getline(std::cin, input);
+                    std::stringstream ss(input);
+                    int value;
+                    
+                    if (!(ss >> value) || !ss.eof()) {
+                        std::cout << "Invalid input! Please enter an integer (without decimal point).\n\n";
+                        type_error = true;
+                    }
+                    else {
+                        safe_diameter = value;
+                    }
+                }
+
+                if (!type_error) {
+                    p.name = safe_name;
+                    p.length = safe_length;
+                    p.diameter = safe_diameter;
+                    std::cout << "Pipe created successfully!\n\n";
+                }                
+                clearInputBuffer();
+            }
             break;
 
         case 2:
